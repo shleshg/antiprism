9ccbb742c21180bdff91a6e5f4b1acf6e0b7b6b0
Oleg Shatnyuk <o.shatnuk@gmail.com>
2020-04-28 18:00:05 +0300

    drop generate

--- generate/server.js
+++
@@ -1,6 +0,0 @@
-const antiprism = require('antiprism');
-const client = require('./client');
-(async () => {
-    const provider = await client.NewProvider('./config.json');
-    new antiprism.HttpServer('/api/antiprism', provider);
-})();
